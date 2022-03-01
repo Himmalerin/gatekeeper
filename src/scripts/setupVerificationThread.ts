ff33c09073ed6f73078451826dec4e57d21a52b8
Ophelia Anderson <git@ophelia.ink>
2022-03-01 16:33:10 -0600

    wrap everything in a try...catch

--- src/scripts/setupVerificationThread.ts
+++ src/scripts/setupVerificationThread.ts
@@ -3,30 +3,35 @@ import {verification} from "../../config.json";
 import {userMention} from "@discordjs/builders";
 
 export default async (client: Client, member: GuildMember): Promise<void> => {
-    const verificationChannel = await client.channels.fetch(verification.channelId) as TextChannel;
+    try {
+        const verificationChannel = await client.channels.fetch(verification.channelId) as TextChannel;
 
-    const verificationThreads: FetchedThreads = await verificationChannel.threads.fetchActive();
+        const verificationThreads: FetchedThreads = await verificationChannel.threads.fetchActive();
 
-    const previousVerificationThread = verificationThreads.threads.find((thread) => thread.name === `verify-${member.user.id}`);
+        const previousVerificationThread = verificationThreads.threads.find((thread) => thread.name === `verify-${member.user.id}`);
 
-    if (previousVerificationThread === undefined) {
-        const thread = await verificationChannel.threads.create({
-            name: `verify-${member.user.id}`,
-            autoArchiveDuration: 60,
-        });
+        if (previousVerificationThread === undefined) {
+            const thread = await verificationChannel.threads.create({
+                name: `verify-${member.user.id}`,
+                autoArchiveDuration: 60,
+            });
 
-        // Automatically delete the "thread started" message
-        const threadMessage = await verificationChannel.messages.fetch(thread.id);
-        await threadMessage.delete();
+            // Automatically delete "thread started" messages since we'll be creating a *lot* of those
+            // Unfortunately has the side effect of creating ghost unread dots
+            const threadMessage = await verificationChannel.messages.fetch(thread.id);
+            await threadMessage.delete();
 
-        await thread.send({
-            content: `Welcome to the WoF Fanon Wiki verification process, ${userMention(member.user.id)}>! Click the link below to get started and then send your Fandom username in this thread.
+            await thread.send({
+                content: `Welcome to the WoF Fanon Wiki verification process, ${userMention(member.user.id)}>! Click the link below to get started and then send your Fandom username in this thread.
 https://community.fandom.com/wiki/Special:VerifyUser?c=+&user=${encodeURIComponent(member.user.username)}&tag=${member.user.discriminator}`,
-        });
-    } else {
-        await previousVerificationThread.send({
-            content: `Welcome back to the WoF Fanon Wiki verification process, ${userMention(member.user.id)}! Click the link below to get started and then send your Fandom username in this thread.
+            });
+        } else {
+            await previousVerificationThread.send({
+                content: `Welcome back to the WoF Fanon Wiki verification process, ${userMention(member.user.id)}! Click the link below to get started and then send your Fandom username in this thread.
 https://community.fandom.com/wiki/Special:VerifyUser?c=+&user=${encodeURIComponent(member.user.username)}&tag=${member.user.discriminator}`,
-        });
+            });
+        }
+    } catch (e) {
+        console.error(e);
     }
 };
